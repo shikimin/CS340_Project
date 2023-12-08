009a3dc191fb1a8592c77296b540b7730fa7f16a
Minyoung Shin <mshin794@gmail.com>
2023-12-07 19:42:53 -0800

    moved functions from load_cats to move_text

--- catopia-database-system/public/js/move_text.js
+++ catopia-database-system/public/js/move_text.js
@@ -12,7 +12,6 @@ function moveText(data, destinations) {
         if (document.getElementById(destinations[i]).tagName == "SELECT") {
             dropdownOptions = document.getElementById(destinations[i]).options;
             for (let j = 0; j < dropdownOptions.length; j++) {
-                console.log(dropdownOptions[j]);
                 if (dropdownOptions[j].innerHTML == data[i]) {
                     dropdownOptions[j].selected = true;
                   break;
@@ -28,8 +27,48 @@ function moveText(data, destinations) {
         else {
             document.getElementById(destinations[i]).value = data[i];
         }
+    };
+    
+    // for updating cat dropdown on update Reservation form
+    if (destinations[0] == 'res_id_update') {
+        customer_id = document.getElementById(destinations[1]).value;
+        console.log(customer_id);
+        loadCats(customer_id, 'cat-dropdown-update');
     }
 
     // Snap to form
     document.getElementById(destinations[0]).scrollIntoView();
+};
+
+/**
+ * load cats with same customer ID when user choose a customer
+ * @param {*} customerID 
+ */
+function loadCats(customerID, tableID) {
+    const link = '/get-cats/' + customerID;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', link, true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            // load cats to the drop down menu
+            updateCatDropdown(xhttp.response, tableID);
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log('There was an error with the input.');
+        }
+        
+    };
+    xhttp.send();
+};
+
+function updateCatDropdown(data, tableID) {
+    let parsedData = JSON.parse(data);
+    let catDropdown = document.getElementById(tableID);
+    let catDropdownHTML = '';
+
+    for (let i = 0; i < parsedData.cats.length; i++) {
+        catDropdownHTML += '<option value="' + parsedData.cats[i].cat_id + '">' + parsedData.cats[i].cat_id + '. ' + parsedData.cats[i].cat_name + '</option>';
+    }
+    catDropdown.innerHTML = catDropdownHTML;
 };
